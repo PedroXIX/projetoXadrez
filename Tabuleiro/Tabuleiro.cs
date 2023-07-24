35826adc4da13ea5010c09c5377b4ae4b766f509
Pedro Silva <pedrohrsilva2004@hotmail.com>
2023-07-23 23:45:47 -0300

    Comecando a implementar o tabuleiro

---
+++ Tabuleiro/Tabuleiro.cs
@@ -0,0 +1,20 @@
+﻿namespace tabuleiro
+{
+    class Tabuleiro
+    {
+        public int Linhas { get; set; }
+        public int Colunas { get; set; }
+        private Peca[,] _pecas { get; set; }
+
+        public Tabuleiro()
+        {
+        }
+
+        public Tabuleiro(int linhas, int colunas)
+        {
+            Linhas = linhas;
+            Colunas = colunas;
+            _pecas = new Peca[Linhas, Colunas];
+        }
+    }
+}
